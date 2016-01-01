fca5c439fc5ccfdfd87d115a37ac2c8c92277ffd
“vigurigit config --global user.email “manoloviguri@gmail.com <manoloviguri@gmail.com>
2016-01-01 04:18:45 +0100

    añadimos capacidades al proyecto

---
+++ PLDSpace/ThirdViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ThirdViewController.swift
+//  PLDSpace
+//
+//  Created by Manuel Emilio Rodríguez de Viguri Fernández on 1/1/16.
+//  Copyright © 2016 Manuel Emilio Rodríguez de Viguri Fernández. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
